165fa18236f48214e1ad244b211382438ec067c2
Search14 <143705208+Search14@users.noreply.github.com>
2023-08-31 12:28:54 +0200

    Update streamlit_app.py
    nueva opción

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My parents'' new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
